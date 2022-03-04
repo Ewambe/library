1d3caa99066158d6bf14db049a29aa773db07f28
Ewambe <100327960+Ewambe@users.noreply.github.com>
2022-03-04 14:51:20 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,12 @@
+def newGit(repo)
+{
+  git "${repo}"
+}
 
+def newMaven()
+{
+  sh 'mvn package'
+}
+def newDeploy(ip,appname)
+deploy adapters: [tomcat9(credentialsId: '24c93484-c125-4361-aa30-fcfe38f0264b', path: '', url: "${ip}")], contextPath: "{appname}", war: '**/*.war'    
+} 
